7074dc0afcc9d08fab71332f4fdad519c3770e23
Finn Evermann <finnevermann@gmx.de>
2020-06-15 10:20:30 +0200

    -test

---
+++ src/components/SavedWorkout.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="mx-2 mt-2" @click="showDetails = !showDetails">
+    <v-card-title> {{ workout.name }} </v-card-title>
+    <v-card-subtitle> {{ formatDate(workout.date) }}</v-card-subtitle>
+    <v-card-text v-if="showDetails">
+      details
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  props: ["workout"],
+  data() {
+    return {
+      showDetails: false
+    };
+  },
+  methods: {
+    formatDate(date) {
+      return moment(date)
+        .locale("de")
+        .calendar();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <h2>Verlauf</h2>
+    <h2 class="mt-3 text-center font-weight-regular">Verlauf</h2>
+    <div v-if="savedWorkouts.length">
+      <SavedWorkout
+        v-for="workout in savedWorkouts"
+        :key="workout.date"
+        :workout="workout"
+      />
+    </div>
+    <div v-else>Du hast noch keine Trainingseinheiten gespeichert</div>
   </div>
 </template>
 
 <script>
+import SavedWorkout from "../components/SavedWorkout";
 export default {
-  //
+  components: {
+    SavedWorkout
+  },
+
+  computed: {
+    savedWorkouts() {
+      const savedWorkouts = localStorage.getItem("savedWorkouts")
+        ? JSON.parse(localStorage.getItem("savedWorkouts"))
+        : [];
+
+      return savedWorkouts;
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.v-expansion-panel-header {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.date {
+  color: rgb(136, 136, 136);
+}
+</style>
