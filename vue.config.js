051a1e1301023fdb8426358d747d1a678ddf3d0e
finn-ev <finnevermann@gmx.de>
2021-06-24 23:22:14 +0200

    vue.config fix

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,5 @@ module.exports = {
     iconPaths: {
       favicon32: "favicon.ico"
     }
-  },
-  icons: [
-    {
-      src: "./logo192.png",
-      sizes: "192x192",
-      type: "image/png",
-      purpose: "any maskable"
-    }
-  ]
+  }
 };
