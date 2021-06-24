409f4aa08242e568c901b8709819bec8ba498456
finn-ev <finnevermann@gmx.de>
2021-06-24 23:19:41 +0200

    pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    iconPaths: {
+      favicon32: "favicon.ico"
+    }
+  },
+  icons: [
+    {
+      src: "./logo192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any maskable"
+    }
+  ]
 };
