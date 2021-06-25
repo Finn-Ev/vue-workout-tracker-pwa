0d546c6b881e584b5224ce0d2502ee98a91028ac
finn-ev <finnevermann@gmx.de>
2021-06-25 10:55:34 +0200

    workout-data-fix

--- src/workout-data/workouts.js
+++ src/workout-data/workouts.js
@@ -187,7 +187,7 @@ export default [
       {
         name: "Frenchpress",
         sets: 3,
-        reps: "8-10",
+        reps: "10",
         pause: "1.5",
         intervall: "1, 1, 1, 0",
         bodyweight: false
@@ -195,7 +195,7 @@ export default [
       {
         name: "Trizpesdrücken einarmig",
         sets: 4,
-        reps: "10-12",
+        reps: "12",
         pause: "1",
         intervall: "2, 0, 1, 1",
         bodyweight: false
@@ -297,7 +297,7 @@ export default [
       {
         name: "Ausfallschritte",
         sets: 2,
-        reps: "8-10",
+        reps: "10",
         pause: "1.5",
         intervall: "1, 0, 1, 0",
         bodyweight: false
@@ -360,7 +360,7 @@ export default [
       {
         name: "Trizeps Pushdowns (Seil)",
         sets: 3,
-        reps: "8-10",
+        reps: "10",
         pause: "1.5",
         intervall: "1, 0, 1, 1",
         bodyweight: false
@@ -415,7 +415,7 @@ export default [
       {
         name: "Hammer Curls",
         sets: 3,
-        reps: "8-10",
+        reps: "10",
         pause: "1.5",
         intervall: "1, 0, 1, 1",
         bodyweight: false
@@ -423,7 +423,7 @@ export default [
       {
         name: "Bizepscurls KH",
         sets: 4,
-        reps: "10-12",
+        reps: "12",
         pause: "1",
         intervall: "1, 0, 1, 1",
         bodyweight: false
@@ -470,7 +470,7 @@ export default [
       {
         name: "Bulg. Split Squats",
         sets: 2,
-        reps: "8-10",
+        reps: "10",
         pause: "1.5",
         intervall: "1, 0, 1, 0",
         bodyweight: false
@@ -478,7 +478,7 @@ export default [
       {
         name: "Wadenheben (sitzend)",
         sets: 4,
-        reps: "10-12",
+        reps: "12",
         pause: "1.5",
         intervall: "1, 0, 1, 0",
         bodyweight: false
@@ -541,7 +541,7 @@ export default [
       {
         name: "Seitheben (Maschine)",
         sets: 4,
-        reps: "10-12",
+        reps: "12",
         pause: "1",
         intervall: "1, 0, 1, 0",
         bodyweight: false
@@ -706,7 +706,7 @@ export default [
       {
         name: "Ausfallschritte",
         sets: 2,
-        reps: "8-10",
+        reps: "10",
         pause: "1.5",
         intervall: "1, 0, 1, 0",
         bodyweight: false
