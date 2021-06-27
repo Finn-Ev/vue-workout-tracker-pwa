a5f920faba2b1142f7ee10bc256ade0464b3acb8
finn-ev <finnevermann@gmx.de>
2021-06-27 18:59:52 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: "Workout-App",
+    themeColor: "#ffffff",
+    msTileColor: "#ffffff",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the manifest options
+    manifestOptions: {
+      display: "landscape",
+      background_color: "#ffffff"
+    },
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js"
+      // ...other Workbox options...
+    }
+  }
 };
